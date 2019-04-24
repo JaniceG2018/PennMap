6895a9cbfa991d34d152a33cff96492ec8c43fef
calchen01 <calchen@nichols-ve504-0141.apn.wlan.upenn.edu>
2019-04-24 12:43:48 -0400

    added some locations

--- src/GraphTest.java
+++ src/GraphTest.java
@@ -8,10 +8,16 @@ public class GraphTest {
 	@Test
 	public void testFindShortestPath() {
 		List<Location> locations = new ArrayList<Location>();
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Huntsman Hall", "school", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Inn at Penn", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Van Pelt Library", "library", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Bookstore", "bookstore", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sansom Place East", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Starbucks", "coffee shop", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("International House", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sheraton University City", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("AT&T Store", "telecom", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("LaTao Hot Pot", "restaurant", new Coordinate(1.0, 1.0)));
 		
 		List<Road> roads = new ArrayList<Road>();
 		roads.add(new Road("start", "end", "name", 1.0));
--- src/PennMapTest.java
+++ src/PennMapTest.java
@@ -17,10 +17,16 @@ public class PennMapTest {
 	@Test
 	public void testMakeQuadTree() {
 		List<Location> locations = new ArrayList<Location>();
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Huntsman Hall", "school", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Inn at Penn", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Van Pelt Library", "library", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Bookstore", "bookstore", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sansom Place East", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Starbucks", "coffee shop", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("International House", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sheraton University City", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("AT&T Store", "telecom", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("LaTao Hot Pot", "restaurant", new Coordinate(1.0, 1.0)));
 		
 		BaseNode root = new InternalNode(new LeafNode(), new LeafNode(), new LeafNode(),
 				new LeafNode());
@@ -33,16 +39,28 @@ public class PennMapTest {
 		//shouldn't this take more than a list of location names as we are building a
 		//road network???
 		List<String> locNames = new ArrayList<String>();
-		locNames.add("location name");
-		locNames.add("location name");
-		locNames.add("location name");
-		locNames.add("location name");
+		locNames.add("Huntsman Hall");
+		locNames.add("Inn at Penn");
+		locNames.add("Van Pelt Library");
+		locNames.add("Bookstore");
+		locNames.add("Sansom Place East");
+		locNames.add("Starbucks");
+		locNames.add("International House");
+		locNames.add("Sheraton University City");
+		locNames.add("AT&T Store");
+		locNames.add("LaTao Hot Pot");
 		
 		List<Location> locations = new ArrayList<Location>();
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
-		locations.add(new Location("name", "type", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Huntsman Hall", "school", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Inn at Penn", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Van Pelt Library", "library", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Bookstore", "bookstore", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sansom Place East", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Starbucks", "coffee shop", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("International House", "dorm", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("Sheraton University City", "hotel", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("AT&T Store", "telecom", new Coordinate(1.0, 1.0)));
+		locations.add(new Location("LaTao Hot Pot", "restaurant", new Coordinate(1.0, 1.0)));
 		
 		List<Road> roads = new ArrayList<Road>();
 		roads.add(new Road("start", "end", "name", 1.0));
