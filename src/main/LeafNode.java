c2a4f1b84b060fa59156e86ac620f80700920fb1
JaniceGJY <gjy.janicecarol@gmail.com>
2019-04-27 00:38:03 -0400

    Changed LeafNode

--- src/main/LeafNode.java
+++ src/main/LeafNode.java
@@ -1,12 +1,20 @@
 package main;
 
 import java.util.List;
+import java.util.Spliterator;
+
+import org.junit.validator.PublicClassValidator;
 
 public class LeafNode extends BaseNode {
 
 	@Override
 	public void search(String type, Range range, List<Location> locs) {
 		
+		
+	}
+	
+	public InternalNode split() {
+		return null;
 	}
 	
 	
