1e0ab06bbb136a498b08a1a24489a839c6149e5e
calchen01 <calchen@vagelos-ve501-1007.apn.wlan.upenn.edu>
2019-04-27 20:04:08 -0400

    wrote Range tests

--- src/test/RangeTest.java
+++ src/test/RangeTest.java
@@ -15,12 +15,22 @@ public class RangeTest {
 		Range range = new Range(new Coordinate(0, 0), new Coordinate(100, 100));
 		Coordinate inRange = new Coordinate(50, 50);
 		Coordinate outOfRange = new Coordinate(150, 150);
+		assertFalse(range.contains(null));
 		assertTrue(range.contains(inRange));
 		assertFalse(range.contains(outOfRange));
 	}
 	
 	@Test
 	public void testIntersects() {
-		
+		Range thisRange = new Range(new Coordinate(0, 0), new Coordinate(100, 100));
+		Range intersectedRange = new Range(new Coordinate(50, 50), new Coordinate(150, 150));
+		Range separateRange = new Range(new Coordinate(150, 150), new Coordinate(200, 200));
+		Range nullRange = null;
+		assertFalse(thisRange.intersects(null));
+		assertFalse(nullRange.intersects(thisRange));
+		assertTrue(thisRange.intersects(intersectedRange));
+		assertTrue(intersectedRange.intersects(thisRange));
+		assertFalse(thisRange.intersects(separateRange));
+		assertFalse(separateRange.intersects(thisRange));
 	}
 }
