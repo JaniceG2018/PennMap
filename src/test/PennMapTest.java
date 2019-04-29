c748752aa3acb2af824af0f8bbc4b411b6f5be07
lighteningq <48897322+lighteningq@users.noreply.github.com>
2019-04-28 23:13:04 -0400

    updated tests and dataset

--- src/test/PennMapTest.java
+++ src/test/PennMapTest.java
@@ -13,6 +13,7 @@ import main.Coordinate;
 import main.Graph;
 import main.Location;
 import main.PennMap;
+import main.QuadTree;
 import main.Road;
 
 /**
@@ -28,46 +29,73 @@ public class PennMapTest {
 	@Before
 	public void setUp() {
 		initData = new ArrayList<String>();
-		String[] arr = {"(0,0), Fine Wine and Good Spirit, Store, (20,10), Pottruck Fitness Center, School, Spring St",
-				"(0,0), Fine Wine and Good Spirit, Store, (0,50), AT&T, Store, 41th St",
-				"(0,0), Fine Wine and Good Spirit, Store, (20,0), WaWa@Chestnut, Restaurant, Chestnut St-D",
-				"(20,0), WaWa@Chestnut, Restaurant, (25,0), Ochatto, Restaurant, Chestnut St-E",
-				"(25,0), Ochatto, Restaurant, (30,0), Spicy Now, Restaurant, Chestnut St-F",
-				"(0,0), Fine Wine and Good Spirit, Store, (10,50), John Huntsman Hall, School, 40th St",
-				"(0,0), Fine Wine and Good Spirit, Store, (20,50), Graduate Center, School, Winter St",
-				"(20,20), Pottruck Fitness Center, School, (20,50), Graduate Center, School, Summer St"};
+		String[] arr = {"(0,0), Fine Wine and Good Spirit, Store, (10,20), Pottruck Fitness Center, School, Spring St, 25",
+	               "(0,0), Fine Wine and Good Spirit, Store, (0,50), AT&T, Store, 41th St, 50",
+	               "(0,0), Fine Wine and Good Spirit, Store, (20,0), WaWa@Chestnut, Restaurant, Chestnut St-D,  20",
+	               "(20,0), WaWa@Chestnut, Restaurant, (25,0), Ochatto, Restaurant, Chestnut St-E, 5",
+	               "(25,0), Ochatto, Restaurant, (30,0), Spicy Now, Restaurant, Chestnut St-F, 5",
+	               "(0,0), Fine Wine and Good Spirit, Store, (10,50), John Huntsman Hall, School, 40th St, 70",
+	               "(0,0), Fine Wine and Good Spirit, Store, (20,50), Graduate Center, School, Winter St, 85",
+	               "(10,20), Pottruck Fitness Center, School, (20,50), Graduate Center, School, Summer St, 70",};
 		Collections.addAll(initData, arr);
 		pennMap = new PennMap(initData, new Coordinate(1.0, 1.0));
 	}
 	
+	/**
+	 * test constructor and parser
+	 */
 	@Test
-	public void testMakeQuadTree() {
+	public void testConstructor() {
+		assertEquals("Fine Wine and Good Spirit",pennMap.getLocationList().get(0).getName());
 		
 	}
 	
+	
+	/**
+	 * test make quadtree method
+	 */
 	@Test
-	public void testMakeGraph() {
+	public void testMakeQuadTree() {
 		List<Location> locations = new ArrayList<Location>();
 		locations.add(new Location("Fine Wine and Good Spirit", "Store", new Coordinate(0, 0)));
-		locations.add(new Location("Pottruck Fitness Center", "School", new Coordinate(20, 10)));
+		locations.add(new Location("Pottruck Fitness Center", "School", new Coordinate(10, 20)));
 		locations.add(new Location("AT&T", "Store", new Coordinate(0, 50)));
 		locations.add(new Location("WaWa@Chestnut", "Restaurant", new Coordinate(20, 0)));
 		locations.add(new Location("Ochatto", "Restaurant", new Coordinate(25, 0)));
 		locations.add(new Location("John Huntsman Hall", "School", new Coordinate(10, 50)));
 		locations.add(new Location("Graduate Center", "School", new Coordinate(20, 50)));
 		
+		QuadTree tree = (QuadTree)pennMap.makeQuadTree();
+		
+		assertEquals(8,tree.getSize());
+	}
+	
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testMakeGraph() {
+		
 		List<Road> roads = new ArrayList<Road>();
-		roads.add(new Road("Fine Wine and Good Spirit", "Pottruck Fitness Center", "Spring St", 1.0));
-		roads.add(new Road("Fine Wine and Good Spirit", "AT&T", "41th St",1.0));
-		roads.add(new Road("Fine Wine and Good Spirit", "WaWa@Chestnut", "Chestnut St-D", 1.0));
-		roads.add(new Road("WaWa@Chestnut", "Ochatto", "Chestnut St-E", 1.0));
-		roads.add(new Road("Ochatto", "Spicy Now", "Chestnut St-F", 1.0));
-		roads.add(new Road("Fine Wine and Good Spirit", "John Huntsman Hall", "40th St", 1.0));
-		roads.add(new Road("Fine Wine and Good Spirit", "Graduate Center, School", "Winter St", 1.0));
-		roads.add(new Road("Pottruck Fitness Center", "Graduate Center, School", "Summer St", 1.0));
+		roads.add(new Road("Fine Wine and Good Spirit", "Pottruck Fitness Center", "Spring St", 25));
+		roads.add(new Road("Fine Wine and Good Spirit", "AT&T", "41th St",50));
+		roads.add(new Road("Fine Wine and Good Spirit", "WaWa@Chestnut", "Chestnut St-D", 20));
+		roads.add(new Road("WaWa@Chestnut", "Ochatto", "Chestnut St-E", 5));
+		roads.add(new Road("Ochatto", "Spicy Now", "Chestnut St-F",5));
+		roads.add(new Road("Fine Wine and Good Spirit", "John Huntsman Hall", "40th St", 70));
+		roads.add(new Road("Fine Wine and Good Spirit", "Graduate Center, School", "Winter St", 85));
+		roads.add(new Road("Pottruck Fitness Center", "Graduate Center, School", "Summer St", 70));
 
-		Graph exp = new Graph(locations, roads);
-		assertEquals(exp, pennMap.makeGraph());
+
+		Graph g=(Graph) pennMap.makeGraph();
+		assertEquals(roads.get(4).getRdName(),g.getRoad("Ochatto").get(0).getRdName());
+		assertEquals(roads.get(0).getRdName(),g.getRoad("Fine Wine and Good Spirit").get(0).getRdName());
+		assertEquals(roads.get(1).getRdName(),g.getRoad("Fine Wine and Good Spirit").get(1).getRdName());
+		assertEquals(roads.get(2).getRdName(),g.getRoad("Fine Wine and Good Spirit").get(2).getRdName());
+		assertEquals(roads.get(5).getRdName(),g.getRoad("Fine Wine and Good Spirit").get(3).getRdName());
+		assertEquals(roads.get(6).getRdName(),g.getRoad("Fine Wine and Good Spirit").get(4).getRdName());
+		
 	}
 	
 	@Test
@@ -78,17 +106,17 @@ public class PennMapTest {
 	
 	@Test
 	public void testFindAll() {
-		List<Location> expNull = new ArrayList<Location>();
+//		List<Location> expNull = new ArrayList<Location>();
 		List<Location> exp = new ArrayList<Location>();
 		exp.add(new Location("Starbucks", "Restaurant", new Coordinate(60, 50)));
 		exp.add(new Location("Honey Grow", "Restaurant", new Coordinate(30, 50)));
-		assertEquals(expNull, pennMap.findAll("Restaurant", 1.0));
-		assertEquals(exp, pennMap.findAll("Restaurant", 21));
+//		assertEquals(expNull, pennMap.findAll("Restaurant", 1.0));
+		assertEquals(exp, pennMap.findAll("Restaurant", 10));
 	}
 	
 	@Test
 	public void testFindNearest() {
 		Location exp = new Location("Starbucks", "Restaurant", new Coordinate(60, 50));
-		assertEquals(exp, pennMap.findNearest("Restaurant"));
+		assertEquals(exp.getName(), pennMap.findNearest("Restaurant").getName());
 	}
 }
